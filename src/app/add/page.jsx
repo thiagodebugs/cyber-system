dba32267605abc0304beb11442b23b4e78ddf1f0
Thiago Pereira Mendonca <thiagomendonca09@gmail.com>
2023-09-27 18:37:46 -0400

    Update add page

--- src/app/add/page.jsx
+++ src/app/add/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Navbar } from "@/components";
+import { AvatarUpload, Navbar } from "@/components";
 import {
   Toolbar,
   Typography,
@@ -8,13 +8,26 @@ import {
   Stack,
   Button,
   Container,
+  Box,
+  TextField,
 } from "@mui/material";
+import { useState } from "react";
 
 export default function Add() {
+  const [values, setValues] = useState({ name: "", document: "" });
+
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value.toUpperCase(),
+    });
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("submit");
   };
+
   return (
     <>
       <Navbar option="add">
@@ -29,13 +42,39 @@ export default function Add() {
               Novo Associado
             </Typography>
           </Toolbar>
-          <Container component="form" onSubmit={handleSubmit}>
-            <Stack direction="row" spacing={2} justifyContent="flex-end">
-              <Button variant="outlined">Voltar</Button>
-              <Button variant="outlined">Limpar</Button>
-              <Button type="submit" variant="contained">
-                Adicionar
-              </Button>
+          <Container component="form" maxWidth="md" onSubmit={handleSubmit}>
+            <Stack spacing={2}>
+              <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+              >
+                <AvatarUpload />
+                <TextField
+                  label="Nome"
+                  name="name"
+                  fullWidth
+                  required
+                  value={values.name}
+                  onChange={handleChange}
+                />
+                <TextField
+                  label="CPF"
+                  name="document"
+                  fullWidth
+                  required
+                  value={values.document}
+                  onChange={handleChange}
+                />
+              </Stack>
+              <Stack direction="row" spacing={2} justifyContent="flex-end">
+                <Button variant="outlined">Voltar</Button>
+                <Button variant="outlined">Limpar</Button>
+                <Button type="submit" variant="contained">
+                  Adicionar
+                </Button>
+              </Stack>
             </Stack>
           </Container>
         </Paper>
