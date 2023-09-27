a438fcbf6c3990b37dace19709d3b80bb04a36fd
Thiago Pereira Mendonca <thiagomendonca09@gmail.com>
2023-09-27 12:07:39 -0400

    Update home page

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,7 +1,42 @@
+"use client";
+
+import { Card, Navbar, Search } from "@/components";
+import { Box, Grid } from "@mui/material";
+
+import dataMock from "@/MOCK_DATA.json";
+import { useState } from "react";
+
 export default function Home() {
+  const [data, setData] = useState(dataMock);
+
+  const handleSearch = (event) => {
+    const value = event.target.value.toLowerCase();
+    const filter = dataMock.filter((item) => {
+      return (
+        item.name.toLowerCase().includes(value) || item.document.includes(value)
+      );
+    });
+    setData(filter);
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Navbar option="home">
+        <Box sx={{ width: "100%", marginBottom: "15px" }}>
+          <Search handleSearch={handleSearch} />
+        </Box>
+        <Grid container spacing={2}>
+          {data.map((item) => (
+            <Grid item xs={12} md={6} lg={4} key={item.id}>
+              <Card
+                name={item.name}
+                document={item.document}
+                avatar_url={item.avatar_url}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Navbar>
+    </>
   );
 }
