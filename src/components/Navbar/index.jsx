1ed47edd679cfa673b7031982a4ad5f5fa85c4e4
Thiago Pereira Mendonca <thiagomendonca09@gmail.com>
2023-09-27 22:00:41 -0400

    Update to logout

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -14,11 +14,14 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import HomeIcon from "@mui/icons-material/Home";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import LogoutIcon from "@mui/icons-material/Logout";
 import { useState } from "react";
 import NextLink from "next/link";
+import { Fade, LinearProgress, Tooltip } from "@mui/material";
 
 export default function Navbar({ children, option }) {
   const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -29,63 +32,90 @@ export default function Navbar({ children, option }) {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" open={open}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{
-              marginRight: 5,
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Cyber System
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer anchor="left" open={open} onClose={handleDrawerClose}>
-        <Box
-          sx={{ width: 250 }}
-          role="presentation"
-          onClick={handleDrawerClose}
-          onKeyDown={handleDrawerClose}
-        >
-          <List>
-            <ListItem disablePadding>
-              <ListItemButton
-                component={NextLink}
-                to="/home"
-                selected={"home" === option}
-              >
-                <ListItemIcon>
-                  <HomeIcon />
-                </ListItemIcon>
-                <ListItemText primary="Home" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton
-                component={NextLink}
-                to="/add"
-                selected={"add" === option}
-              >
-                <ListItemIcon>
-                  <PersonAddIcon />
-                </ListItemIcon>
-                <ListItemText primary="Novo Associado" />
-              </ListItemButton>
-            </ListItem>
-          </List>
-        </Box>
-      </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        {children}
+    <>
+      <Box
+        sx={{
+          width: "100%",
+          height: "2px",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          zIndex: 2,
+        }}
+      >
+        <Fade in={loading}>
+          <LinearProgress />
+        </Fade>
       </Box>
-    </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" open={open}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{
+                marginRight: 5,
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Cyber System
+            </Typography>
+            <form action="/auth/signout" method="post">
+              <Tooltip title="Sair">
+                <IconButton
+                  color="inherit"
+                  type="submit"
+                  onClick={() => setLoading(true)}
+                >
+                  <LogoutIcon />
+                </IconButton>
+              </Tooltip>
+            </form>
+          </Toolbar>
+        </AppBar>
+        <Drawer anchor="left" open={open} onClose={handleDrawerClose}>
+          <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={handleDrawerClose}
+            onKeyDown={handleDrawerClose}
+          >
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton
+                  component={NextLink}
+                  to="/home"
+                  selected={"home" === option}
+                >
+                  <ListItemIcon>
+                    <HomeIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Home" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton
+                  component={NextLink}
+                  to="/add"
+                  selected={"add" === option}
+                >
+                  <ListItemIcon>
+                    <PersonAddIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Novo Associado" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Box>
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          {children}
+        </Box>
+      </Box>
+    </>
   );
 }
